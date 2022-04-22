7e053d2b82d837d20d357b804c260bdce59a7686
sinnamon-roll <38896242+sinnamon-roll@users.noreply.github.com>
2022-04-22 04:24:22 -0700

    Added code journal 1 session 1

---
+++ sesh1.py
@@ -0,0 +1,20 @@
+# Going through code journal exercises
+# This program will print out a little bit
+# about me.
+
+def PrintInfo():
+	print("My name is Gavin Thompson, my preferred pronouns are he/him.")
+	print("My favorite movie is Avatar by James Cameron, I liked the blue people.")
+	print("My favorite food is hotpot, specifically Chinese hotpot.")
+
+
+#This defines out main()
+#funtion for out program
+def main():
+	PrintInfo()
+
+
+#When we run the program,
+#this exectutes first
+if __name__=="__main__":
+	main()
